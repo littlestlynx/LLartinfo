ef14deacc14aa0c94041dbd4d07a8d3d9a83001f
nivashmi <mnieves50@knights.ucf.edu>
2020-03-19 20:33:22 -0400

    Updates

---
+++ precache-manifest.54f0415f504ec3627c7ccb183899997f.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3924710aea7b50d75669fa66e1d50fc6",
+    "url": "/LLartinfo/index.html"
+  },
+  {
+    "revision": "2838ec6fb284b587551b",
+    "url": "/LLartinfo/static/css/2.e49e09ab.chunk.css"
+  },
+  {
+    "revision": "180ec1ff92427645f6cd",
+    "url": "/LLartinfo/static/css/main.030361d1.chunk.css"
+  },
+  {
+    "revision": "2838ec6fb284b587551b",
+    "url": "/LLartinfo/static/js/2.06182d7f.chunk.js"
+  },
+  {
+    "revision": "b4f259d4a0729cc1bc59fbbe563f4bf3",
+    "url": "/LLartinfo/static/js/2.06182d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "180ec1ff92427645f6cd",
+    "url": "/LLartinfo/static/js/main.18cb0636.chunk.js"
+  },
+  {
+    "revision": "afb50760ef8e74a63ff0",
+    "url": "/LLartinfo/static/js/runtime-main.a480d3e7.js"
+  },
+  {
+    "revision": "4cbed13e7252815b9694b6d4908159d6",
+    "url": "/LLartinfo/static/media/full-1.4cbed13e.png"
+  },
+  {
+    "revision": "bfacdd83b3237a58e04c6aa943c0edc4",
+    "url": "/LLartinfo/static/media/half-1.bfacdd83.png"
+  },
+  {
+    "revision": "71bf4dc5575ea7da04fbd8c85d9980f7",
+    "url": "/LLartinfo/static/media/half-2.71bf4dc5.png"
+  },
+  {
+    "revision": "2baf254bca3edde5cedcf55991c59214",
+    "url": "/LLartinfo/static/media/icon-1.2baf254b.png"
+  },
+  {
+    "revision": "acd844f03adc78ee0857ccf9dbc79ed8",
+    "url": "/LLartinfo/static/media/icon-2.acd844f0.png"
+  },
+  {
+    "revision": "f832bed73b579ac79f8f111527480cf6",
+    "url": "/LLartinfo/static/media/ref-1.f832bed7.png"
+  },
+  {
+    "revision": "5d937d9dc64e37aebef06a8aaef0e465",
+    "url": "/LLartinfo/static/media/ref-2.5d937d9d.png"
+  }
+]);
